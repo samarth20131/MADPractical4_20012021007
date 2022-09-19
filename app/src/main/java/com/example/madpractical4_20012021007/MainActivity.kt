26ff7be84d232edda436024789a77600926ef28e
samarth20131 <samrajgupta3110@gmail.com>
2022-09-19 22:22:12 +0530

    App Working with defaults values in browse and call and alarm button

--- app/src/main/java/com/example/madpractical4_20012021007/MainActivity.kt
+++ app/src/main/java/com/example/madpractical4_20012021007/MainActivity.kt
@@ -17,56 +17,55 @@ import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("SuspiciousIndentation")
+//    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val number :Long = 9998445018
-
+//     Browse Button
         val buttonBrowse = findViewById<Button>(R.id.button_browse)
-//
-//        val editText1 = findViewById<EditText>(R.id.edit_text_web_url)
         buttonBrowse.setOnClickListener {
 //            val editText1 = findViewById<TextInputEditText>(R.id.edit_text_web_url)
 //        val url = editText1.text
             Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://sites.google.com/ganpatuniversity.ac.in/mad/home")).apply { startActivity(this) }
         }
 
+    //  Call Button
         val buttonCall = findViewById<Button>(R.id.button_Call)
-
         buttonCall.setOnClickListener {
 //            val editText2 = findViewById<TextInputEditText>(R.id.edit_text_call_number)
 //            val number = editText2.text.toString()
             Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:9724670034")).apply { startActivity(this) }
         }
 
+//      Contact Button
         val buttoncontact = findViewById<Button>(R.id.button_Contact)
         buttoncontact.setOnClickListener { 
             Intent(Intent.ACTION_VIEW).setType(ContactsContract.Contacts.CONTENT_TYPE).apply { startActivity(this) }
         }
 
+//      Call Log Button
         val buttoncalllog = findViewById<Button>(R.id.button_Call_Log)
         buttoncalllog.setOnClickListener {
             Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).apply { startActivity(this) }
         }
 
+//      Gallery Button
         val buttongallery = findViewById<Button>(R.id.button_Gallery)
         buttongallery.setOnClickListener{
             Intent(Intent.ACTION_VIEW).setType("image/*").apply { startActivity(this) }
         }
 
+//      Camera Button
         val buttoncamera = findViewById<Button>(R.id.button_Camera)
         buttoncamera.setOnClickListener {
             Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply { startActivity(this) }
         }
 
+//      Alarm Button
         val buttonalarm = findViewById<Button>(R.id.button_Alarm)
         buttonalarm.setOnClickListener {
-            Intent(AlarmClock.ACTION_SET_ALARM).apply { startActivity(this) }
-
-//            intent.putExtra(AlarmClock.EXTRA_HOUR,parts[0])
-//            intent.putExtra(AlarmClock.EXTRA_MINUTES,parts[1])
+            Intent(AlarmClock.ACTION_SHOW_ALARMS).apply { startActivity(this) }
 
         }
     }
