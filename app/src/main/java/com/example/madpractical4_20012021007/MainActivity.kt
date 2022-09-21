74e7610e7758919a9cb60cfcb6b13051a7a45d19
samarth20131 <samrajgupta3110@gmail.com>
2022-09-21 14:08:36 +0530

    Practical 4 Complete

--- app/src/main/java/com/example/madpractical4_20012021007/MainActivity.kt
+++ app/src/main/java/com/example/madpractical4_20012021007/MainActivity.kt
@@ -8,62 +8,64 @@ import android.provider.AlarmClock
 import android.provider.CallLog
 import android.provider.ContactsContract
 import android.provider.MediaStore
-import android.widget.Button
-
+import com.example.madpractical4_20012021007.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-
-//    @SuppressLint("SuspiciousIndentation")
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//     Browse Button
-        val buttonBrowse = findViewById<Button>(R.id.button_browse)
-        buttonBrowse.setOnClickListener {
-//            val editText1 = findViewById<TextInputEditText>(R.id.edit_text_web_url)
-//        val url = editText1.text
-            Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://sites.google.com/ganpatuniversity.ac.in/mad/home")).apply { startActivity(this) }
+        binding.buttonBrowse.setOnClickListener {
+            Intent(Intent.ACTION_VIEW)
+                .setData(Uri.parse("https://${binding.url.text.toString()}"))
+                .apply {
+                    startActivity(this)
+                }
         }
 
-    //  Call Button
-        val buttonCall = findViewById<Button>(R.id.button_Call)
-        buttonCall.setOnClickListener {
-//            val editText2 = findViewById<TextInputEditText>(R.id.edit_text_call_number)
-//            val number = editText2.text.toString()
-            Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:9724670034")).apply { startActivity(this) }
+        binding.buttonCall.setOnClickListener {
+            Intent(Intent.ACTION_DIAL)
+                .setData(Uri.parse("tel:${binding.number.text.toString()}"))
+                .apply {
+                    startActivity(this)
+                }
         }
-
-//      Contact Button
-        val buttoncontact = findViewById<Button>(R.id.button_Contact)
-        buttoncontact.setOnClickListener { 
-            Intent(Intent.ACTION_VIEW).setType(ContactsContract.Contacts.CONTENT_TYPE).apply { startActivity(this) }
+        binding.buttonContact.setOnClickListener {
+            Intent(Intent.ACTION_VIEW)
+                .setType(ContactsContract.Contacts.CONTENT_TYPE)
+                .apply {
+                    startActivity(this)
+                }
         }
+        binding.buttonCallLog.setOnClickListener {
+            Intent(Intent.ACTION_VIEW)
+                .setType(CallLog.Calls.CONTENT_TYPE)
+                .apply {
+                    startActivity(this)
+                }
 
-//      Call Log Button
-        val buttoncalllog = findViewById<Button>(R.id.button_Call_Log)
-        buttoncalllog.setOnClickListener {
-            Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).apply { startActivity(this) }
         }
-
-//      Gallery Button
-        val buttongallery = findViewById<Button>(R.id.button_Gallery)
-        buttongallery.setOnClickListener{
-            Intent(Intent.ACTION_VIEW).setType("image/*").apply { startActivity(this) }
+        binding.buttonGallery.setOnClickListener {
+            Intent(Intent.ACTION_VIEW)
+                .setType("image/*")
+                .apply {
+                    startActivity(this)
+                }
         }
-
-//      Camera Button
-        val buttoncamera = findViewById<Button>(R.id.button_Camera)
-        buttoncamera.setOnClickListener {
-            Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply { startActivity(this) }
+        binding.buttonCamera.setOnClickListener {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                .apply {
+                    startActivity(this)
+                }
         }
-
-//      Alarm Button
-        val buttonalarm = findViewById<Button>(R.id.button_Alarm)
-        buttonalarm.setOnClickListener {
-            Intent(AlarmClock.ACTION_SHOW_ALARMS).apply { startActivity(this) }
-
+        binding.buttonAlarm.setOnClickListener {
+            Intent(AlarmClock.ACTION_SHOW_ALARMS)
+                .apply {
+                    startActivity(this)
+                }
         }
     }
 }
